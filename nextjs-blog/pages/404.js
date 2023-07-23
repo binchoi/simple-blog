a71f0772698f4ff81702a2550fe03501496a46b3
Bin Choi <binchoi@u.yale-nus.edu.sg>
2023-07-23 17:33:36 +0900

    Add

---
+++ nextjs-blog/pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <h1 className={utilStyles.headingXl}>404 - Page Not Found</h1>
+        </Layout>
+    );
+}
